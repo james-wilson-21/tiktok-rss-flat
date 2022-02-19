81071e3357275f36b53bfbeb0d536a2556ce9ceb
james-opotiki <treppiejames@hotmail.com>
2022-02-20 11:30:30 +1300

    Still experimenting

--- postprocessing.py
+++ postprocessing.py
@@ -37,7 +37,7 @@ with open('subscriptions.csv') as f:
         updated=None
 
         for tiktok in api.user(username=user).videos(count=count):
-            fe = fg.add_entry()
+            fe = fg.add_entry().insert(0)
             link = "https://tiktok.com/@" + user + "/video/" + tiktok.id
             fe.id(link)
             ts = datetime.fromtimestamp(tiktok.as_dict['createTime'], timezone.utc)
